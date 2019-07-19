34c96ac816ee8841ef0c01d3a4e976558fa22a9a
AJ <ahumphries102@gmail.com>
2019-07-19 11:45:48 -0500

    Updates

---
+++ precache-manifest.9cef3b3981aa1322d31e3d238f9cc543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd230fa1bb9eb48268aa",
+    "url": "/learnmagic/static/js/runtime~main.e5096f11.js"
+  },
+  {
+    "revision": "98656addbf2e8c27d357",
+    "url": "/learnmagic/static/js/main.d15bef76.chunk.js"
+  },
+  {
+    "revision": "3e8f8020f14e444ed185",
+    "url": "/learnmagic/static/js/2.bd7bbea0.chunk.js"
+  },
+  {
+    "revision": "98656addbf2e8c27d357",
+    "url": "/learnmagic/static/css/main.3ef3a8f1.chunk.css"
+  },
+  {
+    "revision": "a0029487f291793962dca4b600c42561",
+    "url": "/learnmagic/index.html"
+  }
+];
